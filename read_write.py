4f7247ef10ee2a9befe53dd34c885155d663b1b2
Egor Maleev <e9126515199@icloud.com>
2022-02-04 15:02:23 +0500

    FINAL

--- read_write.py
+++ read_write.py
@@ -63,7 +63,6 @@ def get_shop_list_by_dishes(dishes, person_count):
             slovar_2 = {}
             slovar_2['quantity'] = 0
             if ingridient['ingridient_name'] in slovar:
-                # slovar_2['quantity'] = slovar['ingridient_name']['quantity']
                 slovar[ingridient['ingridient_name']]['quantity'] += int(ingridient['quantity']) * person_count
             else:
                 slovar_2['measure'] = ingridient['measure']
